6424218629dba221483605ae32c881c93e1366da
saki <16546851+sakiko-2@users.noreply.github.com>
2020-03-17 00:00:34 +1300

    sort

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Search } from 'react-feather';
-import { setTextFilter } from '../actions';
+import {
+  setTextFilter,
+  sortByCurrency,
+  sortByRate,
+} from '../actions';
+import filterStyles from './Filter.module.css';
 
 const Filter = (props) => {
+  const [dropdownState, setDropdownState] = useState({ active: false });
+
+  const showDropdown = dropdownState.active ? 'is-active' : '';
+
+  const toggleDropdown = () => {
+    setDropdownState({ active: !dropdownState.active });
+  }
+
   const onTextChange = (e) => {
     props.setTextFilter(e.target.value);
   };
 
   return (
-    <>
+    <div className="container">
       <div className="field">
         <p className="control has-icons-right">
           <input
@@ -24,10 +37,31 @@ const Filter = (props) => {
           </span>
         </p>
       </div>
-      <div>
-        Sort by <strong>{props.sortBy}</strong>
+      <div className="has-text-centered">
+        {'Sort by '}
+        <div className={`dropdown ${showDropdown}`}>
+          <div className="dropdown-trigger">
+            <a
+              aria-haspopup="true"
+              aria-controls="dropdown-menu3"
+              onClick={() => toggleDropdown()}
+            >
+              <span className={filterStyles.sort}>{props.sortBy}</span>
+            </a>
+          </div>
+          <div className="dropdown-menu" id="dropdown-menu3" role="menu">
+            <div className="dropdown-content">
+              <a href="#" className="dropdown-item" onClick={() => { props.sortByCurrency(); toggleDropdown() }}>
+                Currency
+              </a>
+              <a href="#" className="dropdown-item" onClick={() => { props.sortByRate(); toggleDropdown() }}>
+                Rate
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
@@ -37,6 +71,8 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
+  sortByCurrency: () => dispatch(sortByCurrency()),
+  sortByRate: () => dispatch(sortByRate()),
   setTextFilter: (text) => dispatch(setTextFilter(text)),
 });
 
--- src/components/List.js
+++ src/components/List.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { fetchData } from '../actions';
 import { getFilteredList } from '../selectors/filters';
 import Filter from './Filter';
+import listStyles from './List.module.css';
 
 class List extends Component {
   componentDidMount() {
@@ -10,38 +11,49 @@ class List extends Component {
   }
 
   render() {
-    const { data, date } = this.props;
+    const { data, date, loading } = this.props;
+
+    const listContent = loading ?
+      <div className={listStyles.tableContainer}>
+        <span className="loader is-size-2" /> 
+      </div> :
+      <>
+        <Filter />
+        <div className={listStyles.tableContainer}>
+          <table className={`table is-striped ${listStyles.table}`}>
+            <thead>
+              <tr>
+                <th>Currency</th>
+                <th>Rate</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item, i) => 
+                <tr key={i}>
+                  <td>{item[0]}</td>
+                  <td>{item[1]}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </>;
 
     return (
-      <div>
-        <h3 className="subtitle is-3">
+      <section className={`section ${listStyles.list}`}>
+        <h3 className="subtitle is-3 has-text-centered">
           New Zealand Dollar<br/>Foreign Exchange Rates: {date}
         </h3>
-        <Filter />
-        <table className="table is-striped">
-          <thead>
-            <tr>
-              <th>Currency</th>
-              <th>Rate</th>
-            </tr>
-          </thead>
-          <tbody>
-           {data.map((item, i) => 
-              <tr key={i}>
-                <td>{item[0]}</td>
-                <td>{item[1]}</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
+        {listContent}
+      </section>
     );
-  };
+  }
 };
 
 const mapStateToProps = (state) => ({
   data: getFilteredList(state.rates.data, state.filter),
   date: state.rates.date,
+  loading: state.rates.loading,
 });
 
 const mapDispatchToProps = (dispatch) => ({
