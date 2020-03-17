b48ea5edccfe10e86210ab527c471bc08c59bcc0
saki <16546851+sakiko-2@users.noreply.github.com>
2020-03-18 02:02:08 +1300

    Updates

---
+++ precache-manifest.0b48a2d497b2e539d591089aaab18fe0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f3e1225ca52111ee50c3e433c1b647",
+    "url": "/exchange-rates-react-redux/index.html"
+  },
+  {
+    "revision": "2bbc728ced472f625ca7",
+    "url": "/exchange-rates-react-redux/static/css/2.aeed1b96.chunk.css"
+  },
+  {
+    "revision": "d566a83cd822963631bd",
+    "url": "/exchange-rates-react-redux/static/css/main.fbfa6016.chunk.css"
+  },
+  {
+    "revision": "2bbc728ced472f625ca7",
+    "url": "/exchange-rates-react-redux/static/js/2.25f81d8b.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/exchange-rates-react-redux/static/js/2.25f81d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d566a83cd822963631bd",
+    "url": "/exchange-rates-react-redux/static/js/main.c210bb85.chunk.js"
+  },
+  {
+    "revision": "121c87c8d043717b39a0",
+    "url": "/exchange-rates-react-redux/static/js/runtime-main.620408c0.js"
+  }
+]);
